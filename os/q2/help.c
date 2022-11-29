ebc502dabf4a1a73ac411bdc9ce1cd205443b155
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2022-11-30 03:07:53 +0600

    Add files via upload

---
+++ os/q2/help.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include "help.h"
+
+void help() {
+  printf("You are in help panel\n");
+}
---
+++ os/q2/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include "help.h"
+
+int main() {
+  printf("hello everyone\n");
+  help();
+  return 0;
+}
